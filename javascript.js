59de7c2e5ec88e9e2389cbae9635a3f8b21fd09f
Abiy Berhe <abiyberhe@gmail.com>
2023-04-18 12:20:06 +0300

    added modified files of index.html style.css javascrip.js

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('.hamburger');
+const navMenu1 = document.querySelector('.nav-menu1');
+const close = document.querySelector('.close');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu1.classList.toggle('active');
+});
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu1.classList.remove('active');
+}));
+close.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu1.classList.remove('active');
+});
